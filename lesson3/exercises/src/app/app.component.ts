f3a3c95d7d38bc84674a2654acd0d4660843b765
zkrem56 <zkremer702@gmail.com>
2022-10-05 12:27:38 -0500

    Finished setting up the TakeOff, Land, and Abort Mission buttons

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,34 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+
+  handleTakeOff(){
+    let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
+    if (result){
+      this.color = 'blue';
+      this.height = 10000;
+      this.width = 0;
+      this.message = 'Shuttle in flight';
+    }
+  }
+
+  handleLand(){
+    let result = window.confirm('Are you sure you want to land the shuttle?');
+    if (result){
+      this.color = 'green';
+      this.height = 0;
+      this.width = 0;
+      this.message = 'Shuttle has landed';
+    }
+  }
+
+  handleAbortMission(){
+    let result = window.confirm('Are you sure you want to abort the mission?');
+    if (result){
+      this.color = 'red';
+      this.height = 0;
+      this.width = 0;
+      this.message = 'Mission Aborted';
+    }
+  }
 }
