3eab1b71d06d9116b15c110aa5d94a92908f1510
zkrem56 <zkremer702@gmail.com>
2022-10-05 12:41:49 -0500

    Finished movement buttons of exercise

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -42,4 +42,24 @@ export class AppComponent {
       this.message = 'Mission Aborted';
     }
   }
+
+  moveRocket(rocketImage, direction){
+    if (direction === 'right'){
+      let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+    } else if (direction === 'left'){
+      let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width - 10000;
+    } else if (direction === 'up'){
+      let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height = this.height + 10000;
+    } else if (direction === 'down'){
+      let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height = this.height - 10000;
+    }
+  }
 }
